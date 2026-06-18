9fd66fd8e67f069be777bf2151d003901358e233
riddhitrapasiya0 <23amtics156@gmail.com>
2026-06-18 13:57:15 +0530

    Remove Layout and ProtectedRoute components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Layout from "./components/Layout";
-import ProtectedRoute from "./components/ProtectedRoute";
 import ListingsIndex from "./pages/ListingsIndex";
 import ListingShow from "./pages/ListingShow";
 import ListingCreate from "./pages/ListingCreate";
@@ -18,29 +16,14 @@ export default function App() {
       <BrowserRouter>
         <ToastContainer />
         <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Navigate to="/listings" replace />} />
-            <Route path="/listings" element={<ListingsIndex />} />
-            <Route path="/listings/:id" element={<ListingShow />} />
-            <Route
-              path="/listings/new"
-              element={
-                <ProtectedRoute>
-                  <ListingCreate />
-                </ProtectedRoute>
-              }/>
-            <Route
-              path="/listings/:id/edit"
-              element={
-                <ProtectedRoute>
-                  <ListingEdit />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<ErrorPage />} errorElement={<ErrorPage />} />
-          </Route>
+          <Route path="/" element={<Navigate to="/listings" replace />} />
+          <Route path="/listings" element={<ListingsIndex />} />
+          <Route path="/listings/:id" element={<ListingShow />} />
+          <Route path="/listings/new" element={<ListingCreate />} />
+          <Route path="/listings/:id/edit" element={<ListingEdit />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<ErrorPage />} errorElement={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
