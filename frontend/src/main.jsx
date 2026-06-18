f365c58b5283418681ae0160fd0c41c3da1a237f
riddhitrapasiya0 <23amtics156@gmail.com>
2026-06-18 14:02:24 +0530

    Fix import in main.jsx and remove duplicate BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>
+    <ToastContainer
+      position="top-left"
+      autoClose={3000}
+      hideProgressBar={false}
+    />
+  </StrictMode>,
 );
